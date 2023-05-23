3d17deabf8e7b8b07ccf1e0c489edcf6000b8bee
Igor Mattos <igormmattos06@gmail.com>
2023-05-23 20:40:48 -0300

    Configura por evento

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-const form = document.forms.entrada;
-form.addEventListener('submit', envia);
+window.addEventListener('load', setup);
+function setup(){
+    const form = document.forms.entrada;
+    form.addEventListener('submit', envia);
 
-function envia(evento){
-    evento.preventDefault();
-    console.log('Formulário enviado!!')
+    function envia(evento){
+        evento.preventDefault();
+        console.log('Formulário enviado!!')
+    }
 }
