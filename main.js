c29679862512748c770032a5af9b74312042a62b
Igor Mattos <igormmattos06@gmail.com>
2023-05-30 19:27:33 -0300

    adiciona um estado base

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-window.addEventListener('load', setup);
-function setup(){
     const form = document.forms.entrada;
     form.addEventListener('submit', envia);
 
+    let estado = 0;
+
     function envia(evento){
         evento.preventDefault();
-        console.log('Formulário enviado!!')
+        console.log('Formulário enviado!!');
+        estado++;
+        atualiza();
     }
-}
+
+    function atualiza(){
+        const ol = document.querySelector('ol');
+        ol.innerHTML = `<li>${estado}</li>`;
+    }
